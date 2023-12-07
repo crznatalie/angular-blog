c4066ccc25f045b142e86c9aec15cad0f202a9bc
crznatalie <cpnnatalie@gmail.com>
2023-12-07 10:45:06 -0300

    Finalização projeto

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,29 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Herringbone - Greyson Chance",
+    "description": "Title: Herringbone | Sung by: Greyson Chance | Composed by: Greyson Chance, Jason Reeves | Produced by: Jason Reeves",
+    "photoCover": "https://images.squarespace-cdn.com/content/v1/5ccca402fb18203b15c064a9/1663770662413-3MFVZFXZUDUNSRCA3H02/GreysonChance-35mm-17.jpg?format=2500w"
+  },
+
+  {
+    "id":"2",
+    "title": "Carry You - Novo Amor",
+    "description": "Title: Carry You | Sung by: Novo Amor | Composed by: Ali Lacey, Ed Tullett | Produced by: -",
+    "photoCover": "https://assets.papelpop.com/wp-content/uploads/2020/11/Novo-Amor-2-credit-Dan-Harris-for-photo-and-Aline-Brant-for-embroidery1.jpg"
+  },
+
+  {
+    "id":"3",
+    "title": "Revelator Eyes - The Paper Kites",
+    "description": "Title: Revelator Eyes | Sung by: The Paper Kites | Composed by: Samuel Bentley | Produced by: Phil Ek",
+    "photoCover": "https://nettwerk.com/wp-content/uploads/TPK-Roadhouse-Lead-Press-Photo-credit_-Nick-McKinlay-1-scaled.jpg"
+  },
+
+  {
+    "id":"4",
+    "title": "Agnes by Glass Animals",
+    "description": "Title: Agnes | Sung by: Glass Animals | Composed by: Dave Bayley | Produced by: Dave Bayley",
+    "photoCover": "https://www.places-concert.com/images/visuels/artists_glass_animals_04082020165103.jpg"
+  },
+]
